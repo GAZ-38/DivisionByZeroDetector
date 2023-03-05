e0a0f96ee5a61d28560f735dc47028a06a3dc13d
GAZ-38 <126726572+GAZ-38@users.noreply.github.com>
2023-03-06 01:20:14 +0300

    Update DivisionByZeroDetectorAnalyzer.cs

--- DivisionByZeroDetector/DivisionByZeroDetector/DivisionByZeroDetectorAnalyzer.cs
+++ DivisionByZeroDetector/DivisionByZeroDetector/DivisionByZeroDetectorAnalyzer.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -15,10 +16,34 @@ namespace DivisionByZeroDetector
     {
         public const string DiagnosticId = "DivisionByZeroDetector";
 
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Error";
+        
+
+        class VariableChangeData
+        {
+            public ISymbol Name;
+            public int Span;
+            public bool IsZero;
+
+            public VariableChangeData()
+            {
+                this.IsZero = false;
+            }
+            public VariableChangeData(ISymbol name, int span, bool isZero)
+            {
+                this.Name = name;
+                this.Span = span;
+                this.IsZero = isZero;
+            }
+        }
+
+        private List<VariableChangeData> variableChanges = new List<VariableChangeData>();
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, 
             Title, MessageFormat, Category, DiagnosticSeverity.Error, 
@@ -30,32 +55,169 @@ namespace DivisionByZeroDetector
 
         public override void Initialize(AnalysisContext context)
         {
+
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
+            // Analyzers for division
             context.RegisterSyntaxNodeAction(AnalyzeDivideNode, SyntaxKind.DivideExpression);
-            context.RegisterSyntaxNodeAction(AnalyzeAssignmentExpressionNode,
+            context.RegisterSyntaxNodeAction(AnalyzeDivideAssignmentExpressionNode,
                 SyntaxKind.DivideAssignmentExpression);
+            
+            // Analyzers for assignment and declaration
+            context.RegisterSyntaxNodeAction(AnalyzeLocalDeclarationNode, SyntaxKind.LocalDeclarationStatement);
+            context.RegisterSyntaxNodeAction(AnalyzeSimpleAssignmentExpressionNode,
+                SyntaxKind.SimpleAssignmentExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeComplexAssignmentExpressionNode,
+                SyntaxKind.DivideAssignmentExpression,
+                SyntaxKind.MultiplyAssignmentExpression, 
+                SyntaxKind.AddAssignmentExpression,
+                SyntaxKind.SubtractAssignmentExpression);
         }
+
+        private void AnalyzeLocalDeclarationNode(SyntaxNodeAnalysisContext context)
+        {
+            ISymbol name;
+            int span;
+            bool isZero;
+
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            foreach (VariableDeclaratorSyntax variable in localDeclaration.Declaration.Variables)
+            {
+                //Сhecking whether the variable is not initialized
+                EqualsValueClauseSyntax initializer = variable.Initializer;
+                if (initializer == null)
+                {
+                    continue;
+                }
+                //checking whether the variable is initialized by constant
+                Optional <object> constantValue = context.SemanticModel.GetConstantValue(initializer.Value, context.CancellationToken);
+                if (constantValue.HasValue)
+                {
+                    //checking whether the variable is initialized by zero
+                    isZero = constantValue.Value.Equals(0);
+                }
+                else
+                {
+                    continue;
+                }
+
+                name = context.SemanticModel.GetDeclaredSymbol(variable, context.CancellationToken);
+
+                span = variable.SpanStart;
+
+                //Addition of information about the variable to the list of changes
+                VariableChangeData changeData = new VariableChangeData(name, span, isZero);
+                variableChanges.Add(changeData);
+            }
+        }
+
+        private void AnalyzeSimpleAssignmentExpressionNode(SyntaxNodeAnalysisContext context)
+        {
+            ISymbol name;
+            int span;
+            bool isZero;
+
+            var assignmentExpression = (AssignmentExpressionSyntax)context.Node;
+            var constantValue = context.SemanticModel.GetConstantValue(assignmentExpression.Right, context.CancellationToken);
+
+            // Checking whether constant value is assigned to a variable
+            if (!constantValue.HasValue)
+            {
+                return;
+            }
+
+            // Checking whether zero is assigned to a variable
+            isZero = constantValue.Value.Equals(0);
+           
+            name = context.SemanticModel.GetSymbolInfo(assignmentExpression.Left, context.CancellationToken).Symbol;
+
+            span = assignmentExpression.SpanStart;
+
+            //Addition of information about the variable to the list of changes
+            VariableChangeData changeData = new VariableChangeData(name, span, isZero);
+            variableChanges.Add(changeData);
+        }
+
+        private void AnalyzeComplexAssignmentExpressionNode(SyntaxNodeAnalysisContext context)
+        {
+            // If assignment operator is not simple, the value of the zero variable could be changed
+            var assignmentExpression = (AssignmentExpressionSyntax)context.Node;
+
+            ISymbol name = context.SemanticModel.GetSymbolInfo(assignmentExpression.Left, context.CancellationToken).Symbol;
+            int span = assignmentExpression.SpanStart;
+            bool isZero = false;
+
+            //Addition of information about the variable to the list of changes
+            VariableChangeData changeData = new VariableChangeData(name, span, isZero);
+            variableChanges.Add(changeData);
+        }
+
 
         private void AnalyzeDivideNode(SyntaxNodeAnalysisContext context)
         {
-            var binaryExpression = (BinaryExpressionSyntax)context.Node;
+            var divisionExpression = (BinaryExpressionSyntax)context.Node;
 
-            // Checking whether the divisor is not a constant equal to zero
-            if (!context.SemanticModel.GetConstantValue(binaryExpression.Right).Value.Equals(0))
+            if (context.SemanticModel.GetConstantValue(divisionExpression.Right).HasValue.Equals(false))
+            {
+                // Divisor is not a constant:
+                // checking whether the last operation with a divisor is assignment of zero
+                if (divisionExpression.Right.IsKind(SyntaxKind.IdentifierName))
+                {
+                    ISymbol name = context.SemanticModel.GetSymbolInfo(divisionExpression.Right, context.CancellationToken).Symbol;
+                    
+                    int span = divisionExpression.SpanStart;
+                    bool isDividedByZero = false;
+                    foreach (VariableChangeData changeData in variableChanges)
+                    {
+                        if (changeData.Span >= span) break;
+                        if (!SymbolEqualityComparer.Default.Equals(name, changeData.Name)) continue;
+
+                        isDividedByZero = changeData.IsZero;
+                    }
+                    if (!isDividedByZero) { 
+                        return; 
+                    }
+                }
+                else { return; }
+            }
+            // Divisor is a constant: checking whether the divisor is zero
+            else if (!context.SemanticModel.GetConstantValue(divisionExpression.Right).Value.Equals(0))
             {
                 return;
             }
             context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
         }
 
-        private void AnalyzeAssignmentExpressionNode(SyntaxNodeAnalysisContext context)
+        private void AnalyzeDivideAssignmentExpressionNode(SyntaxNodeAnalysisContext context)
         {
-            var assignmentExpression = (AssignmentExpressionSyntax)context.Node;
+            var divisionExpression = (AssignmentExpressionSyntax)context.Node;
 
-            // Checking whether the divisor is not a constant equal to zero
-            if (!context.SemanticModel.GetConstantValue(assignmentExpression.Right).Value.Equals(0))
+            if (context.SemanticModel.GetConstantValue(divisionExpression.Right).HasValue.Equals(false))
+            {
+                // Divisor is not a constant:
+                // checking whether the last operation with a divisor is assignment of zero
+                if (divisionExpression.Right.IsKind(SyntaxKind.IdentifierName))
+                {
+                    ISymbol name = context.SemanticModel.GetSymbolInfo(divisionExpression.Right, context.CancellationToken).Symbol;
+
+                    int span = divisionExpression.SpanStart;
+                    bool isDividedByZero = false;
+                    foreach (VariableChangeData changeData in variableChanges)
+                    {
+                        if (changeData.Span >= span) break;
+                        if (!SymbolEqualityComparer.Default.Equals(name, changeData.Name)) continue;
+
+                        isDividedByZero = changeData.IsZero;
+                    }
+                    if (!isDividedByZero) {
+                        return;
+                    }
+                }
+                else { return; }
+            }
+            // Divisor is a constant: checking whether the divisor is zero
+            else if (!context.SemanticModel.GetConstantValue(divisionExpression.Right).Value.Equals(0))
             {
                 return;
             }
